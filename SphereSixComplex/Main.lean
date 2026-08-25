b0aa3c3be10deb1a23ec37048338dcfec30e87ff
Paul-Lez <paul.lezeau@gmail.com>
2026-08-25 15:30:29 +0000

    reduce clutching extension to collared nullhomotopy

--- SphereSixComplex/Main.lean
+++ SphereSixComplex/Main.lean
@@ -226,6 +226,7 @@ public import SphereSixComplex.Topology.SmoothSixSphereClassification
 public import SphereSixComplex.Topology.SmoothRecognition
 public import SphereSixComplex.Topology.SmoothRecognitionFoundations
 public import SphereSixComplex.Topology.StableFraming
+public import SphereSixComplex.Topology.StableFramingBufferedRadialClutching
 public import SphereSixComplex.Topology.StableFramingHomotopySixSphere
 public import SphereSixComplex.Topology.StableFramingHomotopySixSphereClutching
 public import SphereSixComplex.Topology.StableFramingStandardSixSphereCover
