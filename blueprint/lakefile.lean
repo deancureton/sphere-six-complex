e37fb6a3619e9ede083c77e12f63211249aa296d
Dean Cureton <89793485+deancureton@users.noreply.github.com>
2026-08-25 20:20:34 -0700

    align blueprint toolchain

--- blueprint/lakefile.lean
+++ blueprint/lakefile.lean
@@ -4,9 +4,9 @@ open Lake DSL
 
 require SphereSixComplex from ".."
 require VersoBlueprint from git
-  "https://github.com/leanprover/verso-blueprint.git" @ "v4.33.0"
+  "https://github.com/leanprover/verso-blueprint.git" @ "a441323930138cccf42e34396746af67d72078b6"
 require mathlib from git
-  "https://github.com/Paul-Lez/mathlib4" @ "dba911505e5f973d80c20bb1e3f6952f34081e29"
+  "https://github.com/Paul-Lez/mathlib4" @ "97d303eb50436be7c4bac4388bdb49459ae9140b"
 
 package SphereSixComplexBlueprint where
   precompileModules := false
